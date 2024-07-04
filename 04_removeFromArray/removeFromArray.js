d1d1e846b82c2a2eee5352bf36e0c371d1436358
absolzen <robdomu@gmail.com>
2024-07-04 22:21:42 +0200

    Resolve problem with multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,14 @@
 const removeFromArray = function(array, ...args) {
 
-  function isInArray(arrayValue) { // arrayValue = 1
-    for (const arg of args) { // arg = 3
-      if (arg === arrayValue) {
-        return false; // returns false if argument 
-      } else {
-        return true;
-      }
-    }
+  // const array = arg;
+  // const arrayOfArgs = args;
+  
+  for (const arg of args) {
+    array = array.filter((arrayValue) => arrayValue !== arg);
   }
+
+  return array;
   
-  const newArray = array.filter(isInArray);
-  
-  return newArray;
 };
 
 // Do not edit below this line
