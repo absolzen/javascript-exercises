12d1c40b7213f8978981c7a6491a9e02e03ec031
absolzen <robdomu@gmail.com>
2024-07-04 21:09:48 +0200

    Add function to return array clear of arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+
+  function isInArray(arrayValue) { // arrayValue = 1
+    for (const arg of args) { // arg = 3
+      if (arg === arrayValue) {
+        return false; // returns false if argument 
+      } else {
+        return true;
+      }
+    }
+  }
+  
+  const newArray = array.filter(isInArray);
+  return newArray;
 };
 
 // Do not edit below this line
