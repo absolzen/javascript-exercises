10dc36c97a1adaad6c55815630999f2521429560
absolzen <robdomu@gmail.com>
2024-07-04 22:27:00 +0200

    Cleanup code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,14 +1,10 @@
 const removeFromArray = function(array, ...args) {
 
-  // const array = arg;
-  // const arrayOfArgs = args;
-  
   for (const arg of args) {
     array = array.filter((arrayValue) => arrayValue !== arg);
   }
 
-  return array;
-  
+  return array;  
 };
 
 // Do not edit below this line
