bee4fb9976cfbe1dec33dc8cd68b1408b9b42447
absolzen <robdomu@gmail.com>
2024-07-04 09:28:57 +0200

    Add code to pass first test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  const chars = word.split(''); // Array of chars
+  let reversedWord = chars.reverse().join('');
+  return reversedWord;
 };
 
 // Do not edit below this line
