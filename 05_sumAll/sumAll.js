fe25e556d344535e62cbd48cc9774981259524ad
absolzen <robdomu@gmail.com>
2024-07-05 15:15:36 +0200

    Add handling of below zero and non number args

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,12 +2,20 @@ const sumAll = function(num1, num2) {
 
   let sum = 0;
 
+  if (num1 < 0 ||
+      num2 < 0 ||
+      typeof num1 != "number" ||
+      typeof num2 != "number") {
+    return "ERROR";
+  }
+
   if (num1 > num2) { // reverse nums 
     let numTemp; 
     numTemp = num1;
     num1 = num2;
     num2 = numTemp;
   }
+
 
   for (let i = num1; i <= num2; i++) {
     sum += i;
