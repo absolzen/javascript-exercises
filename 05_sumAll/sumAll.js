131d4a7c891e42683005ab97aca8b7d55d4d8bfe
absolzen <robdomu@gmail.com>
2024-07-05 15:09:11 +0200

    Add sorting of arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,13 @@ const sumAll = function(num1, num2) {
 
   let sum = 0;
 
+  if (num1 > num2) { // reverse nums 
+    let numTemp; 
+    numTemp = num1;
+    num1 = num2;
+    num2 = numTemp;
+  }
+
   for (let i = num1; i <= num2; i++) {
     sum += i;
   }
