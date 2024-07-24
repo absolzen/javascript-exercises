38134964c6a39baa183b04dab25ee321666c265c
absolzen <robdomu@gmail.com>
2024-07-24 13:07:47 +0200

    Add function to check palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let strAlphaNumLower = str.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+  let strRev = strAlphaNumLower.split('').reverse().join('');
+  return strAlphaNumLower === strRev;
 };
 
 // Do not edit below this line
