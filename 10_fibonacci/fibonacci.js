19368299d94d7b2f7dd9cd265b46887a8bc3748d
absolzen <robdomu@gmail.com>
2024-07-24 19:53:00 +0200

    Solve Fibonacci excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  num = +num;
+  let arr = [0];
+  if (num === 0) return arr[0];
+  if (num < 0) return 'OOPS';
 
+  arr.push(1);
+  for (i = 1; i < num; i++) {
+    let result = arr.slice(-2).reduce((sum, current) => sum + current);
+    arr.push(result);
+  }
+  return +arr.slice(-1).toString();
 };
 
 // Do not edit below this line
