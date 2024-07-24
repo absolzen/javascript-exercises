6931a605d7bdffc28d487c4387b83c0fa00777ff
absolzen <robdomu@gmail.com>
2024-07-24 19:54:55 +0200

    Format code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,6 @@
 const palindromes = function (str) {
-  let strAlphaNumLower = str.replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
-  let strRev = strAlphaNumLower.split('').reverse().join('');
+  let strAlphaNumLower = str.replace(/[^a-zA-Z0-9]+/g, "").toLowerCase();
+  let strRev = strAlphaNumLower.split("").reverse().join("");
   return strAlphaNumLower === strRev;
 };
 
